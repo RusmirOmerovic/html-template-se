9da8ff909fd8574a575ccf845a4d1b69f627c34b
Rusmir Omerovic <rusmir.omerovic@web.de>
2025-07-26 11:56:09 +0200

    📦 Update: 2025-07-26 11:56 – Automatischer Push

--- frontend/frontend/src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://dzvxcpsbaqjlcjaqgfit.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6dnhjcHNiYXFqbGNqYXFnZml0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NjE5ODEsImV4cCI6MjA2OTAzNzk4MX0.uguVbv7itOy6Ax-sZQZnlnIZ2wfEHm4bkjEZKIV2uug'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
-
