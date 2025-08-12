fa731b8cae816e6e7e1bfc9c591b9c11f270a2e5
Rusmir Omerovic <rusmir.omerovic@web.de>
2025-08-12 23:25:38 +0200

    chore: add lint scripts for HTML, CSS, JS + fix color name in index.css

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-//change the text "current theme: light" into "current theme: dark"
+// change the text "current theme: light" into "current theme: dark"
 
-const themeIndicator = document.getElementById('theme-indicator');
-const toggleBtn = document.getElementById('toggle-btn');
-let isDarkMode = false;
-toggleBtn.addEventListener("click", () => {
-  isDarkMode = !isDarkMode;
-  document.body.classList.toggle('dark-mode', isDarkMode);
-  themeIndicator.textContent = `Current Theme: ${isDarkMode ? 'Dark' : 'Light'}`;
-  document.body.classList.toggle("dark");
-});
+const themeIndicator = document.getElementById('theme-indicator')
+const toggleBtn = document.getElementById('toggle-btn')
+let isDarkMode = false
+toggleBtn.addEventListener('click', () => {
+  isDarkMode = !isDarkMode
+  document.body.classList.toggle('dark-mode', isDarkMode)
+  themeIndicator.textContent = `Current Theme: ${isDarkMode ? 'Dark' : 'Light'}`
+  document.body.classList.toggle('dark')
+})
